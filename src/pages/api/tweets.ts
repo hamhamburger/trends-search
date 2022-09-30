13513758d5b6ab937275aaba93660203a46d408b
hamham <kerokero2978@gmail.com>
2022-09-30 10:42:52 +0900

    ツイートの情報を取得するAPI追加中

---
+++ src/pages/api/tweets.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import TwitterApi from 'twitter-api-v2'
+
+const appOnlyClient = new TwitterApi(process.env['TWITTER_BEARER_TOKEN'] as string)
+
+const getTweetsInfo = async (keyword: string, client: TwitterApi): Promise<any> => {
+  const tweets = await client.search('JavaScript', { 'media.fields': 'url' })
+
+  return tweets
+}
+
+type Data = {
+  tweets?: any[]
+  message: string
+}
+
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<Data>,
+): Promise<void> {
+  const keyword = _req.query['keyword'] ? _req.query['keyword'].toString() : 'ビットコイン'
+  const result = await getTweetsInfo(keyword, appOnlyClient)
+  if (result) {
+    res.status(200).json(result)
+  } else {
+    res.status(404).json({
+      message: 'Tweets not found',
+    })
+  }
+}
