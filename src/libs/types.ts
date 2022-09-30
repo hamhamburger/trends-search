209f0e5fa16d04e2e571f0879529d49ecee4afb2
hamham <kerokero2978@gmail.com>
2022-09-30 20:56:42 +0900

    グラフを画面に追加（仮）

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,3 +1,17 @@
 export type TrendKewordResult = {
   rank: number
 }
+
+export type AppState = {
+  tweetsCount?: TweetsCount
+}
+
+export type TweetsCount = {
+  total: number
+  data: Array<{ month: number; day: number; hour: number; count: number }>
+}
+
+export type Actions = {
+  type: 'SET_TWITTER_COUNT'
+  payload: { tweetsCount?: TweetsCount }
+}
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 
 import TrendForm from '@/components/form/TrendForm'
+import TweetsGraph from '@/components/graph/TweetsGraph'
 import MyHead from '@/components/MyHead'
 const search: NextPage = () => {
   return (
@@ -9,6 +10,7 @@ const search: NextPage = () => {
 
       <main>
         <TrendForm />
+        <TweetsGraph />
       </main>
 
       <footer></footer>
